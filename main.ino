c7790a1e462254728dd3ad1ee075118557773464
Alexander-James2 <144252140+Alexander-James2@users.noreply.github.com>
2023-09-19 20:51:46 -0400

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,16 @@
+// Created by: Alex James
+// Created on: Sept 2023
+//
+// This is a blink program for the Arduino Uno
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
